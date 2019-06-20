0f4e9878c80873db37f3c58081f3716a73b9ba1f
Yulia <parma2903@gmail.com>
2019-06-20 08:52:17 +0300

    Выполнено задание 1 по лекции 4

--- js/main.js
+++ js/main.js
@@ -18,18 +18,65 @@ var NAMES = [
   'Наташа',
   'Марина',
 ];
+var MIN_SCALE = 0;
+var MAX_SCALE = 100;
+var STEP = 25;
+
 var avatarQuantity = 6;
 var photosQuantity = 25;
+var effects = [
+  'effects__preview--none',
+  'effects__preview--chrome',
+  'effects__preview--sepia',
+  'effects__preview--marvin',
+  'effects__preview--phobos',
+  'effects__preview--heat'
+];
 
 var similarPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 var photosListElement = document.querySelector('.pictures');
 var fragment = document.createDocumentFragment();
 var photosCollection = createPhotosArray(photosQuantity);
-var inputUploadFile = document.querySelector('#upload-file');
+var uploadForm = document.querySelector('#upload-select-image');
+var uploadPreview = document.querySelector('.img-upload__preview');
+var effectRadioList = document.querySelector('.effects__list');
+var effectLevel = uploadForm.querySelector('.effect-level');
+var effectDepth = uploadForm.querySelector('.effect-level__depth');
+var levelPin = uploadForm.querySelector('.effect-level__pin');
+var uploadInput = document.querySelector('#upload-file');
 var uploadOverlay = document.querySelector('.img-upload__overlay');
-var buttonCloseUploadPhoto = document.querySelector('.img-upload__cancel');
-inputUploadFile.addEventListener('change', onInputPhotoUpload);
+var uploadOverlayCloseBt = document.querySelector('.img-upload__cancel');
+var effectLevelValueElement = effectLevel.querySelector('.effect-level__value');
+var imageUploadPreviewElement = uploadPreview.firstElementChild;
+var scaleControlSmaller = uploadOverlay.querySelector('.scale__control--smaller');
+var scaleControlBigger = uploadOverlay.querySelector('.scale__control--bigger');
+var scaleCntrolValue = uploadOverlay.querySelector('.scale__control--value');
+scaleCntrolValue.value = 100 + '%';
+
+
 renderPhotos(photosCollection);
+
+uploadInput.addEventListener('change', function () {
+  openUploadOverlay();
+  effectRadioList.addEventListener('click', chooseEffect);
+});
+
+uploadOverlayCloseBt.addEventListener('click', function () {
+  closeUploadOverlay();
+  effectRadioList.removeEventListener('click', chooseEffect);
+});
+
+levelPin.addEventListener('mouseup', function () {
+  changeIntensityEffect();
+});
+
+scaleControlSmaller.addEventListener('click', function () {
+  outZoom();
+});
+
+scaleControlBigger.addEventListener('click', function () {
+  inZoom();
+});
 
 function getElement(array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -83,25 +130,87 @@ function renderPhotos(photos) {
   photosListElement.appendChild(fragment);
 }
 
-function onUploadOverlayPressEsc(evt) {
+function onUploadOverlayEscPress(evt) {
   if (evt.keyCode === 27) {
     closeUploadOverlay();
   }
 }
 
-function onbuttonCloseUploadPhotoClick() {
-  closeUploadOverlay();
-}
-
-function onInputPhotoUpload() {
+function openUploadOverlay() {
   uploadOverlay.classList.remove('hidden');
-  window.addEventListener('keydown', onUploadOverlayPressEsc);
-  buttonCloseUploadPhoto.addEventListener('click', onbuttonCloseUploadPhotoClick);
+  document.addEventListener('keydown', onUploadOverlayEscPress);
 }
 
 function closeUploadOverlay() {
   uploadOverlay.classList.add('hidden');
-  inputUploadFile.addEventListener('change', onInputPhotoUpload);
-  window.removeEventListener('keydown', onUploadOverlayPressEsc);
-  buttonCloseUploadPhoto.removeEventListener('click', onbuttonCloseUploadPhotoClick);
+  document.removeEventListener('keydown', onUploadOverlayEscPress);
+}
+
+function chooseEffect(evt) {
+  for (var i = 0; i < effects.length; i++) {
+    if (evt.target.value === 'none') {
+      effectLevel.classList.add('hidden');
+    } else {
+      effectLevel.classList.remove('hidden');
+    }
+    if ('effects__preview--' + evt.target.value === effects[i]) {
+      uploadPreview.classList.add('effects__preview--' + evt.target.value);
+    } else {
+      uploadPreview.classList.remove(effects[i]);
+    }
+  }
+
+  effectDepth.style.width = 0;
+  levelPin.style.left = 0;
+}
+
+function changeIntensityEffect() {
+  if (imageUploadPreviewElement.classList === 'effects__preview--chrome') {
+    imageUploadPreviewElement.style.filter =
+      'grayscale(' + effectLevelValueElement.value + ')';
+  } else if (
+    imageUploadPreviewElement.classList === 'effects__preview--sepia'
+  ) {
+    imageUploadPreviewElement.style.filter =
+      'sepia(' + effectLevelValueElement.value + ')';
+  } else if (
+    imageUploadPreviewElement.classList === 'effects__preview--marvin'
+  ) {
+    imageUploadPreviewElement.style.filter =
+      'invert(' + effectLevelValueElement.value * 100 + '%)';
+  } else if (
+    imageUploadPreviewElement.classList === 'effects__preview--phobos'
+  ) {
+    imageUploadPreviewElement.style.filter =
+      'blur(' + effectLevelValueElement.value * 3 + 'px)';
+  } else if (
+    imageUploadPreviewElement.classList === 'effects__preview--heat'
+  ) {
+    imageUploadPreviewElement.style.filter =
+      'brightness(' + effectLevelValueElement.value * 3 + ')';
+  }
+}
+
+function outZoom() {
+  var scaleSmaller = parseInt(scaleCntrolValue.value, 10) - STEP;
+  if (scaleSmaller <= MIN_SCALE) {
+    scaleSmaller = MIN_SCALE;
+    scaleCntrolValue.value = MIN_SCALE + '%';
+  } else {
+    scaleCntrolValue.value = scaleSmaller + '%';
+  }
+
+  imageUploadPreviewElement.style.transform = 'scale(' + scaleSmaller / 100 + ')';
+}
+
+function inZoom() {
+  var scaleBigger = parseInt(scaleCntrolValue.value, 10) + STEP;
+  if (scaleBigger >= MAX_SCALE) {
+    scaleBigger = MAX_SCALE;
+    scaleCntrolValue.value = MAX_SCALE + '%';
+  } else {
+    scaleCntrolValue.value = scaleBigger + '%';
+  }
+
+  imageUploadPreviewElement.style.transform = 'scale(' + scaleBigger / 100 + ')';
 }
